5d6ffc56a4dade2ccd181e339d5f647acd76a98a
parkermuir <parker.muir@gmail.com>
2018-07-15 20:59:04 -0400

    config add

---
+++ server/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: 'localhost',
+  user: 'ur sql username which is probably root', 
+  password: 'ur sql password which is probably nothing', 
+  database: 'badmovies',
+  API_KEY: '7d06008a2e2e990619d128bf606171d1',
+};
